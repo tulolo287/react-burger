7660ff8a5efcfd323eb7eaa711af541a46cabaca
tulolo287 <iv1055@yandex.ru>
2023-07-28 23:40:05 +0300

    folder structure

---
+++ src/components/burger-constructor/BurgerConstructor.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BurgerConstructor = () => {
+  return (
+    <div>BurgerConstructor</div>
+  )
+}
+
+export default BurgerConstructor
---
+++ src/components/burger-ingredients/BurgerIngredients.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BurgerIngredients = () => {
+  return (
+    <div>BurgerIngredients</div>
+  )
+}
+
+export default BurgerIngredients
