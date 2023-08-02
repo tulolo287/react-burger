58cef4f4e8a4208a031eb18844dc7b3f2af9bbdb
tulolo287 <95403916+tulolo287@users.noreply.github.com>
2023-08-02 18:29:55 +0300

    stackblitz

--- src/components/hocs/with-modal.jsx
+++ src/components/hocs/with-modal.jsx
@@ -3,60 +3,58 @@ import {
   ConstructorElement,
   DragIcon,
   CurrencyIcon,
-} from "@ya.praktikum/react-developer-burger-ui-components";
-import React, { useEffect, useState } from "react";
-import styles from "../modal/modal.module.css";
-import { dataTypes } from "../../utils/consts";
-import Modal from "../modal/modal";
+} from '@ya.praktikum/react-developer-burger-ui-components';
+import React, { useEffect, useState } from 'react';
+import styles from '../modal/modal.module.css';
+import { dataTypes } from '../../utils/consts';
+import Modal from '../modal/modal';
 
 import { createPortal } from 'react-dom';
-import OrderDetails from "../order-details/order-details.jsx"
+import OrderDetails from '../order-details/order-details.jsx';
 
-
-
-const withModal = WrappedComponent => {
+const withModal = (WrappedComponent) => {
   return class extends React.Component {
-      constructor(props) {
-          super(props);
+    constructor(props) {
+      super(props);
 
-          this.onButtonClick = this.onButtonClick.bind(this);
-          this.state = {
-              isToggled: this.props.initialToggleState
-          };
-      }
+      this.onButtonClick = this.onButtonClick.bind(this);
+      this.state = {
+        isToggled: this.props.initialToggleState,
+      };
+    }
 
-      onButtonClick() {
-        console.log(this.state)
-          this.setState({
-              isToggled: !this.state.isToggled,
-          });
-      }
+    onButtonClick() {
+      console.log(this.state);
+      this.setState({
+        isToggled: !this.state.isToggled,
+      });
+    }
 
-      render() {
-          const {initialToggleState, ...props} = this.props;
-          // Извлечём все необходимые пропсы.
-          return (
-          <>
+    render() {
+      const { initialToggleState, ...props } = this.props;
+
+      return (
+        <>
           {this.state.isToggled &&
-            createPortal(<>
-              <div onClick={this.onButtonClick} className={styles.modal_bg}></div>
-              <div className={styles.modal_content}><OrderDetails/></div>
-              </>, 
-                  document.body
-                  
-                  )
-              }
-              <WrappedComponent 
-                  {...props}
+            createPortal(
+              <>
+                <div
                   onClick={this.onButtonClick}
-                  
-              >
-                {this.props.children}
-                </WrappedComponent>
-                </>
-          );
-      }
-  }
+                  className={styles.modal_bg}
+                ></div>
+                <div className={styles.modal_content}>
+                  <OrderDetails />
+                </div>
+              </>,
+              document.body
+            )}
+          <WrappedComponent {...props} onClick={this.onButtonClick}>
+            {this.props.children}
+          </WrappedComponent>
+        </>
+      );
+    }
+  };
 };
 
-export default withModal; 
+export default withModal;
