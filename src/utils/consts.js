96bdb4714a6d1256c90a80b8cb71dcdcaea90a9c
tulolo287 <95403916+tulolo287@users.noreply.github.com>
2023-08-04 14:18:35 +0300

    1 file modified

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,33 +1,18 @@
-import {
-  Button,
-  ConstructorElement,
-  DragIcon,
-  CurrencyIcon,
-  CheckMarkIcon,
-} from "@ya.praktikum/react-developer-burger-ui-components";
-import React from "react";
 import PropTypes from "prop-types";
-import styles from './ingredient-details.module.css'
 
-const IngredientDetails = ({ item }) => {
-  return (
-    <section className={styles.ingredientDetails}>
-      <p className="text text_type_main-large">
-        The quick brown fox jumps over the lazy dog.
-      </p>
-      <img src={item.image_large} alt={item.name} />
-      {item.name}
+const DATA_TYPES = {
+   data: PropTypes.arrayOf(
+     PropTypes.shape({
+       type: PropTypes.string.isRequired,
+       name: PropTypes.string.isRequired,
+       price: PropTypes.number.isRequired,
+       text: PropTypes.string.isRequired,
+       isLocked: PropTypes.bool.isRequired,
+       image: PropTypes.string.isRequired,
+     })
+   ),
+ };
 
-      <p className="text text_type_main-medium">
-        The quick brown fox jumps over the lazy dog.
-      </p>
-      <p className="text text_type_main-default text_color_inactive">
-        {item.calories}
-        {item.proteins}
-        {item.fat}
-        {item.carbohyd}
-      </p>
-    </section>
-  );
-};
-export default IngredientDetails;
+ const URL = "https://norma.nomoreparties.space/api/ingredients"
+
+ export {DATA_TYPES, URL}
